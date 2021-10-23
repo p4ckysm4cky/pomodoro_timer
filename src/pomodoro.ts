2c886aebba8ecd00eb32d74b55ba290bcff6fae7
p4ckysm4cky <58573901+p4ckysm4cky@users.noreply.github.com>
2021-10-23 23:34:32 +1300

    seconds to hh:mm:ss seems okay

--- src/pomodoro.ts
+++ src/pomodoro.ts
@@ -1,2 +1,23 @@
-let message :string = "Hello World"
-console.log()
+// Time Class
+function Time(seconds: number = 0) {
+    this.seconds = seconds;
+}
+
+Time.prototype.formated = function () {
+    const original_seconds = this.seconds;
+    let return_time = {
+        hours: Math.floor(original_seconds / 3600),
+        minutes: Math.floor(original_seconds / 60) % 60,
+        seconds: Math.round(original_seconds % 60),
+    };
+    return return_time;
+};
+
+// Test
+let test = new Time();
+let test1 = new Time(1);
+let test2 = new Time(599);
+let test3 = new Time(600);
+let test4 = new Time(3600);
+let test5 = new Time(360009);
+let test6 = new Time(0.2);
